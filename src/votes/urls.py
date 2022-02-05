4685006c60caf004527b7f5af31b96c11d9430e4
Night <xxxtensei@gmail.com>
2022-02-05 16:03:41 +0300

    Clean urlpatterns

--- src/votes/urls.py
+++ src/votes/urls.py
@@ -8,23 +8,5 @@ router.register(r'profiles', views.ProfileViewSet)
 
 urlpatterns = [
 	path('', include(router.urls))
-	# path('votes/', views.VoteList.as_view()),
-	# path('votes/<int:pk>', views.VoteDetail.as_view()),
 
-	# path('votes/<int:profile_id>/votesScore', views.VoteDetail.as_view()),
-	# path('votes/voteFor', views.VoteDetail.as_view()),
-	# path('votes/voteAgainst', views.VoteDetail.as_view()),
-	# path('votes/<int:pk>/votersMeScore', views.VoteDetail.as_view()),
-	# path('votes/<int:pk>/againstMeScore', views.VoteDetail.as_view()),
-	# path('votes/<int:pk>/againstMeScore', views.VoteDetail.as_view()),
-	# path('votes/mostPopular', views.ProfileDetail.as_view()),
-
-	# path('profiles/', views.ProfileList.as_view()),
-	# path('profiles/<int:pk>', views.ProfileDetail.as_view()),
-
-	# path('profiles/getVotesInfo', views.ProfileDetail.as_view()),
-	# path('profiles/getVotersMe', views.ProfileDetail.as_view()),
-	# path('profiles/getAgainstMe', views.ProfileDetail.as_view()),
-	# path('profiles/<int:pk>/votes/addVote', views.ProfileDetail.as_view()),
-	# path('profiles/<int:pk>/votes/deleteVote', views.ProfileDetail.as_view()),
 ]
