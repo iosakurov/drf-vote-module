77a069938a03c2b4e02c25d5f92e9c93cf18be31
Night <xxxtensei@gmail.com>
2022-02-10 15:34:32 +0300

    `refactor(votes):` change tab to space indent
    And remove 'import time' because it is not used

--- src/votes/serializers.py
+++ src/votes/serializers.py
@@ -1,24 +1,23 @@
 from rest_framework import serializers
 from .models import Profile, Vote
-import time
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-	name = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
 
-	class Meta:
-		model = Profile
-		fields = ['id', 'name']
+    class Meta:
+        model = Profile
+        fields = ['id', 'name']
 
-	def get_name(self, obj):
-		return obj.user.username
+    def get_name(self, obj):
+        return obj.user.username
 
 
 class VoteSerializer(serializers.ModelSerializer):
-	voter = ProfileSerializer()
-	candidate = ProfileSerializer()
+    voter = ProfileSerializer()
+    candidate = ProfileSerializer()
 
-	class Meta:
-		model = Vote
-		fields = ['id', 'is_like', 'voter', 'candidate', 'date_first', 'date_modify']
-		read_only_fields = ['voter', 'candidate', 'date_first', 'date_modify']
+    class Meta:
+        model = Vote
+        fields = ['id', 'is_like', 'voter', 'candidate', 'date_first', 'date_modify']
+        read_only_fields = ['voter', 'candidate', 'date_first', 'date_modify']
